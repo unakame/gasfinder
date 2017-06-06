d113740d0fb1cff06eccff562cf536ecf1c05041
Dana Franco <turquoise.ilg@hotmail.com>
2017-06-06 12:26:10 -0500

    setting search bar

--- js/components/header.js
+++ js/components/header.js
@@ -4,8 +4,10 @@ const Header = (update) => {
   const header = $("<header></header>");
   const title = $("<span>Gas Finder</span>");
   const icon = $('<i class="fa fa-chevron-left" aria-hidden="true"></i>');
+  const input = $('<div class="row"><div class="input-field col m6 s12"><i class="material-icons">search</i><input type="text" placeholder="          Ingresa tu distrito a buscar" data-length="10"></div></div>');
 
   header.append(title);
+  title.append(input);
 
   return header;
 }
